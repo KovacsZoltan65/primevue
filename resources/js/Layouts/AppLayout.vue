915d2204acb864210077ad8667592a730a559467
Kovács Zoltán <kovacs.zoltan@softc.hu>
2024-08-09 13:46:12 +0200

    first commit

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { ref, watch, computed } from 'vue';
+import { useLayout } from '../Layouts/composables/layout';
+
+import AppTopBar from './AppTopBar.vue';
+import AppSidebar from './AppSidebar.vue';
+import AppFooter from './AppFooter.vue';
+
+const { layoutConfig, layoutState, isSidebarActive, resetMenu } = useLayout();
+
+const outsideClickListener = ref(null);
+
+watch(isSidebarActive, (newVal) => {
+    if (newVal) {
+        bindOutsideClickListener();
+    } else {
+        unbindOutsideClickListener();
+    }
+});
+
+const containerClass = computed(() => {
+    return {
+        'layout-overlay': layoutConfig.menuMode === 'overlay',
+        'layout-static': layoutConfig.menuMode === 'static',
+        'layout-static-inactive': layoutState.staticMenuDesktopInactive && layoutConfig.menuMode === 'static',
+        'layout-overlay-active': layoutState.overlayMenuActive,
+        'layout-mobile-active': layoutState.staticMenuMobileActive
+    };
+});
+
+function bindOutsideClickListener() {
+    if (!outsideClickListener.value) {
+        outsideClickListener.value = (event) => {
+            if (isOutsideClicked(event)) {
+                resetMenu();
+            }
+        };
+        document.addEventListener('click', outsideClickListener.value);
+    }
+}
+
+function unbindOutsideClickListener() {
+    if (outsideClickListener.value) {
+        document.removeEventListener('click', outsideClickListener);
+        outsideClickListener.value = null;
+    }
+}
+
+function isOutsideClicked(event) {
+    const sidebarEl = document.querySelector('.layout-sidebar');
+    const topbarEl = document.querySelector('.layout-menu-button');
+
+    return !(sidebarEl.isSameNode(event.target) || sidebarEl.contains(event.target) || topbarEl.isSameNode(event.target) || topbarEl.contains(event.target));
+}
+
+</script>
+
+<template>
+    <div class="layout-wrapper" :class="containerClass">
+        <AppTopBar></AppTopBar>
+
+        <AppSidebar></AppSidebar>
+
+        <div class="layout-main-container">
+            <div class="layout-main">
+                <slot />
+            </div>
+
+            <AppFooter></AppFooter>
+
+        </div>
+
+        <div class="layout-mask animate-fadein"></div>
+        
+    </div>
+</template>
---
+++ resources/js/Layouts/AppSidebar.vue
@@ -0,0 +1,10 @@
+<script setup>
+import AppMenu from './AppMenu.vue';
+</script>
+
+<template>
+    <div class="layout-sidebar">
+        AppSidebar
+        <AppMenu></AppMenu>
+    </div>
+</template>

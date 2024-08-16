6e39082f03beb19ed8eb10223d8f3bdab7dabc9a
Kovács Zoltán <kovacs.zoltan@softc.hu>
2024-08-16 13:32:13 +0200

    mentés

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('language', 5)->default('hu')->comment('Nyelv');
             $table->rememberToken();
             $table->timestamps();
         });

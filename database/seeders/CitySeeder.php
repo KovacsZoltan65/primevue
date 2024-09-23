42dacd33b02a38899f5ac9755ec8a11b5fc41e80
Kovács Zoltán <kovacs.zoltan@softc.hu>
2024-09-23 14:25:36 +0200

    main - seeder

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -22,17 +22,17 @@ class CitySeeder extends Seeder
         Schema::enableForeignKeyConstraints();
 
         $cities = [
-            ['id' => 955428, 'region_id' => 1555, 'country_id' => 92, 'latitude' => '47.50000000', 'longitude' => '19.08333330', 'name' => 'Budapest',       'active' => 1],
-            ['id' => 958977, 'region_id' => 1562, 'country_id' => 92, 'latitude' => '47.68333330', 'longitude' => '17.63333330', 'name' => 'Győr',           'active' => 1],
-            ['id' => 961049, 'region_id' => 1545, 'country_id' => 92, 'latitude' => '47.68333330', 'longitude' => '17.78333330', 'name' => 'Kecskemét',      'active' => 1],
-            ['id' => 961048, 'region_id' => 1548, 'country_id' => 92, 'latitude' => '46.90000000', 'longitude' => '19.78333330', 'name' => 'Kecskemét',      'active' => 1],
-            ['id' => 955545, 'region_id' => 1546, 'country_id' => 92, 'latitude' => '47.16666670', 'longitude' => '19.80000000', 'name' => 'Cegléd',         'active' => 1],
-            ['id' => 964383, 'region_id' => 1556, 'country_id' => 92, 'latitude' => '48.10000000', 'longitude' => '20.78333330', 'name' => 'Miskolc',        'active' => 1],
-            ['id' => 966284, 'region_id' => 1561, 'country_id' => 92, 'latitude' => '46.08333330', 'longitude' => '18.23333330', 'name' => 'Pécs',           'active' => 1],
-            ['id' => 968014, 'region_id' => 1545, 'country_id' => 92, 'latitude' => '47.68333330', 'longitude' => '16.60000000', 'name' => 'Sopron',         'active' => 1],
-            ['id' => 968570, 'region_id' => 1539, 'country_id' => 92, 'latitude' => '47.20000000', 'longitude' => '18.41666670', 'name' => 'Székesfehérvár', 'active' => 1],
-            ['id' => 969175, 'region_id' => 1549, 'country_id' => 92, 'latitude' => '47.23333330', 'longitude' => '16.61666670', 'name' => 'Szombathely',    'active' => 1],
-            ['id' => 971009, 'region_id' => 1547, 'country_id' => 92, 'latitude' => '47.10000000', 'longitude' => '17.91666670', 'name' => 'Veszprém',       'active' => 1],
+            ['id' =>  1, 'region_id' => 1555, 'country_id' => 92, 'latitude' => '47.50000000', 'longitude' => '19.08333330', 'name' => 'Budapest',       'active' => 1],
+            ['id' =>  2, 'region_id' => 1562, 'country_id' => 92, 'latitude' => '47.68333330', 'longitude' => '17.63333330', 'name' => 'Győr',           'active' => 1],
+            ['id' =>  3, 'region_id' => 1545, 'country_id' => 92, 'latitude' => '47.68333330', 'longitude' => '17.78333330', 'name' => 'Kecskemét',      'active' => 1],
+            ['id' =>  4, 'region_id' => 1548, 'country_id' => 92, 'latitude' => '46.90000000', 'longitude' => '19.78333330', 'name' => 'Kecskemét',      'active' => 1],
+            ['id' =>  5, 'region_id' => 1546, 'country_id' => 92, 'latitude' => '47.16666670', 'longitude' => '19.80000000', 'name' => 'Cegléd',         'active' => 1],
+            ['id' =>  6, 'region_id' => 1556, 'country_id' => 92, 'latitude' => '48.10000000', 'longitude' => '20.78333330', 'name' => 'Miskolc',        'active' => 1],
+            ['id' =>  7, 'region_id' => 1561, 'country_id' => 92, 'latitude' => '46.08333330', 'longitude' => '18.23333330', 'name' => 'Pécs',           'active' => 1],
+            ['id' =>  8, 'region_id' => 1545, 'country_id' => 92, 'latitude' => '47.68333330', 'longitude' => '16.60000000', 'name' => 'Sopron',         'active' => 1],
+            ['id' =>  9, 'region_id' => 1539, 'country_id' => 92, 'latitude' => '47.20000000', 'longitude' => '18.41666670', 'name' => 'Székesfehérvár', 'active' => 1],
+            ['id' => 10, 'region_id' => 1549, 'country_id' => 92, 'latitude' => '47.23333330', 'longitude' => '16.61666670', 'name' => 'Szombathely',    'active' => 1],
+            ['id' => 11, 'region_id' => 1547, 'country_id' => 92, 'latitude' => '47.10000000', 'longitude' => '17.91666670', 'name' => 'Veszprém',       'active' => 1],
         ];
 
         foreach($cities as $city)

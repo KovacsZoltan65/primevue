b45b414c5677d4df6c34c6c758fa801da9659a3c
Kovács Zoltán <kovacs.zoltan@softc.hu>
2024-09-26 11:25:29 +0200

    main - request

--- app/Http/Requests/StoreCityRequest.php
+++ app/Http/Requests/StoreCityRequest.php
@@ -3,11 +3,12 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreCityRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
+     * Határozza meg, hogy a felhasználó jogosult-e erre a kérésre.
      */
     public function authorize(): bool
     {
@@ -15,14 +16,61 @@ class StoreCityRequest extends FormRequest
     }
 
     /**
-     * Get the validation rules that apply to the request.
-     *
+     * Szerezze meg a kérelemre vonatkozó érvényesítési szabályokat.
+     * 
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
         return [
-            //
+            // A város neve karakterlánc, minimum 2, maximum 255 karakter hosszú.
+            // Egyedinek kell lennie a városok nevei között.
+            'name' => ['required', 'string', 'min:2', 'max:255', 'unique:cities,name'],
+            // A város országának azonosítója jelen kell lennie, 
+            // és egész számnak kell lennie.
+            'country_id' => ['required', 'integer'],
+            // A város régiójának azonosítója jelen kell lennie, 
+            // és egész számnak kell lennie.
+            'region_id' => ['required', 'integer'],
+            // A város szélessége tizedesjegy, legfeljebb 10 digit hosszú, 
+            // amiből kettő egész szám.
+            'latitude' => ['decimal:10,2'],
+            // A város hosszúsága tizedesjegy, legfeljebb 10 digit hosszú, 
+            // amiből kettő egész szám.
+            'longitute' => ['decimal:10,2'],
+            // A várost aktívként vagy inaktívként kell megjelölni.
+            'active' => ['required', 'boolean'],
         ];
     }
+    public function messages(): array
+    {
+        return [
+            'name' => [
+                'required' => '',
+                'string' => '',
+                'min' => '',
+                'max' => '',
+                'unique' => '',
+            ],
+            'country_id' => [
+                'required' => '',
+                'integer' => '',
+            ],
+            'region_id' => [
+                'required' => '',
+                'integer' => '',
+            ],
+            'latitude' => [
+                'decimal' => '',
+            ],
+            'longitute' => [
+                'decimal' => '',
+            ],
+            'active' => [
+                'required' => '',
+                'boolean' => '',
+            ],
+        ];
+    }
+    
 }
--- app/Http/Requests/UpdateCityRequest.php
+++ app/Http/Requests/UpdateCityRequest.php
@@ -3,26 +3,57 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateCityRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
+     * Határozza meg, hogy a felhasználó jogosult-e erre a kérésre.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
-     * Get the validation rules that apply to the request.
+     * Szerezze meg a kérelemre vonatkozó érvényesítési szabályokat.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
         return [
-            //
+            // A város neve karakterlánc, minimum 2, maximum 255 karakter hosszú.
+            // Egyedinek kell lennie a városok nevei között a jelenlegi város kivételével.
+            'name' => [
+                'request', 'string', 'min:2', 'max:255', 
+                Rule::unique('cities', 'name')->ignore($this->id),
+            ],
+            // Az ország azonosítójának jelen kell lennie, és egész számnak kell lennie.
+            'country_id' => ['required', 'integer'],
+            // A régió azonosítójának jelen kell lennie, és egész számnak kell lennie.
+            'region_id' => ['required', 'integer'],
+            // A város szélessége tizedesjegy, legfeljebb 10 digit hosszú, 
+            // amiből kettő egész szám.
+            'latitude' => ['decimal:10,2'],
+            // A város hosszúsága tizedesjegy, legfeljebb 10 digit hosszú, 
+            // amiből kettő egész szám.
+            'longitute' => ['decimal:10,2'],
+            // A várost aktívként vagy inaktívként kell megjelölni.
+            'active' => ['required', 'boolean'],
         ];
     }
+    
+    public function messages(): array
+    {
+        return [
+            'name' => '',
+            'country_id' => '',
+            'region_id' => '',
+            'latitude' => '',
+            'longitute' => '',
+            'active' => '',
+        ];
+    }
+    
 }

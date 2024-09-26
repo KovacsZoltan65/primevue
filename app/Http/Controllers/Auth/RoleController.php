acc2beeb0ee2a2664eec1436a48494cad2b75b17
Kovács Zoltán <kovacs.zoltan@softc.hu>
2024-09-26 07:04:48 +0200

    main - roles

--- app/Http/Controllers/Auth/RoleController.php
+++ app/Http/Controllers/Auth/RoleController.php
@@ -3,16 +3,19 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\RoleResource;
 use App\Models\Auth\Permission;
-use Illuminate\Http\Request;
+use App\Models\Auth\Role;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class RoleController extends Controller
 {
     public function index(Request $request)
     {
-        $users = \App\Models\User::orderBy('name')->get()->toArray();
+        $users = User::orderBy('name')->get()->toArray();
         $permissions = Permission::orderBy('name')->get()->toArray();
         
         return Inertia::render('Auth/Role/Index', [
@@ -24,6 +27,40 @@ class RoleController extends Controller
     
     public function applySearch(Builder $query, string $search)
     {
-        //
+        return $query->when($search, function($query, string $search) {
+            // A lekérdezéshez hozzáadja a keresési feltételt
+            $query->where('name', 'LIKE', "%{$search}%");
+        });
+    }
+    
+    public function getRoles(Request $request)
+    {
+        $roleQuery = Role::search($request);
+        $roles = RoleResource::collection($roleQuery->get());
+        
+        return $roles;
+    }
+    
+    public function createRole(Request $request)
+    {
+        $role = Role::create($request->all());
+        
+        return response()->json($role, Response::HTTP_OK);
+    }
+    
+    public function updateRole(Request $request, int $id)
+    {
+        $old_role = Role::find($id);
+        $success = $old_role->update($request->all());
+        
+        return response()->json(['success' => $success], Response::HTTP_OK);
+    }
+    
+    public function deleteRole(int $id)
+    {
+        $old_role = Role::find($id);
+        $success = $old_role->delete();
+        
+        return response()->json(['success' => $success], Response::HTTP_OK);
     }
 }

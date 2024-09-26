c08e3ae2b8d6f1ba22f640989428744228d475e9
Kovács Zoltán <kovacs.zoltan@softc.hu>
2024-09-26 14:52:13 +0200

    main - enum in migration

--- database/migrations/2024_09_25_143857_create_subdomain_table.php
+++ database/migrations/2024_09_25_143857_create_subdomain_table.php
@@ -11,21 +11,31 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('subdomain', function (Blueprint $table) {
+        Schema::create('subdomains', function (Blueprint $table) {
             $table->increments('id')->comment('Rekord azonosító');
             $table->string('subdomain')->comment('Példány');
-            $table->string('url')->comment('Példány url-je');
-            $table->string('name')->comment('Aldomain neve');
-            $table->string('db_host', 125)->default('localhost')->comment('Adatbázis szerver címe');
+            $table->string('url')->collation('utf8mb3_general_ci')->comment('Példány url-je');
+            $table->string('name')->collation('utf8mb3_general_ci')->comment('Aldomain neve');
+            $table->string('db_host', 125)->collation('utf8mb3_general_ci')->default('localhost')->comment('Adatbázis szerver címe');
             $table->integer('db_port')->default(3306)->comment('Adatbázis port');
-            $table->string('db_name')->comment('Adatbázis neve');
-            $table->string('db_user')->comment('Adatbázis felhasználó');
-            $table->string('db_password')->comment('Adatbázis jelszó');
-            $table->boolean('notification')->comment('Értesítés');
+            $table->string('db_name')->collation('utf8mb3_general_ci')->comment('Adatbázis neve');
+            $table->string('db_user')->collation('utf8mb3_general_ci')->comment('Adatbázis felhasználó');
+            $table->string('db_password')->collation('utf8mb3_general_ci')->comment('Adatbázis jelszó');
+            
+            //$table->tinyInteger('notification')->comment('Értesítés');
+            $table->enum('notification', ['0','1'])->comment('Értesítés');
+            
             $table->unsignedInteger('state_id')->index('subdomains_state_id_foreign')->comment('Állapot azonosító');
-            $table->tinyInteger('is_mirror')->default(0)->comment('Tükör adatbázis');
-            $table->tinyInteger('sso')->default(0)->comment('SSO');
-            $table->integer('access_control_system')->default(0)->comment('Access Controll System');
+            
+            //$table->tinyInteger('is_mirror')->default(0)->comment('Tükör adatbázis');
+            $table->enum('is_mirror', ['0','1'])->default(0)->comment('Tükör adatbázis');
+            
+            //$table->tinyInteger('sso')->default(0)->comment('SSO');
+            $table->enum('sso', ['0','1'])->default(0)->comment('SSO');
+            
+            //$table->tinyInteger('access_control_system')->default(0)->comment('Access Controll System');
+            $table->enum('access_control_system', ['0','1'])->default(0)->comment('Access Controll System');
+            
             $table->timestamp('last_export')->nullable()->comment('Utoldó export');
             
             $table->timestamps();

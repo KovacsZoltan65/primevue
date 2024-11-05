0a195a134be088a8f8994aedf735561985107af8
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2024-11-05 16:14:19 +0100

    main - mentés

--- resources/js/css_digital_clock.js
+++ resources/js/css_digital_clock.js
@@ -8,25 +8,24 @@ https://frontbackgeek.com/category/html-css/
 	<p id="time"></p>
 </div>
 
+/**
+ * Updates the current time and date displayed on the webpage.
+ *
+ * This function retrieves the current date and time, formats them using
+ * the `zeroPadding` function, and updates the inner text of HTML elements
+ * with IDs 'time' and 'date'.
+ */
 function updateTime() {
-	var now = new Date();
-	
-	document.getEmlementById('time').innerText = 
-		zeroPadding(now.getHours(), 2) + ":" +
-		zeroPadding(now.getMinutes(), 2) + ":" +
-		zeroPadding(now.getSeconds(), 2);
-		
-	document.getElementById('date').innerText = 
-		now.getFullYear() + "." + 
-		zeroPadding(now.getMonth() + 1, 2) + "." + 
-		zeroPadding(now.getDate(), 2) + " " + 
-		WEEK(now.getDay());
+    const now = new Date();
+    const time = `${zeroPadding(now.getHours(), 2)}:${zeroPadding(now.getMinutes(), 2)}:${zeroPadding(now.getSeconds(), 2)}`;
+    const date = `${now.getFullYear()}.${zeroPadding(now.getMonth() + 1, 2)}.${zeroPadding(now.getDate(), 2)} ${WEEK(now.getDay())}`;
+
+    document.getElementById('time').innerText = time;
+    document.getElementById('date').innerText = date;
 }
 
 updateTime();
 
 setInterval(updateTime, 1000);
 
-function zeroPadding(num, digit) {
-	return String(num).padStart(digit, '0');
-}
+const zeroPadding = (num, digit) => num.toString().padStart(digit, '0');

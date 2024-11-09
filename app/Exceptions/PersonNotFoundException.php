cbfb21910a5736bf3e05aa583a534ef7126c0758
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2024-11-09 18:27:13 +0100

    persons - PersonNotFoundException

---
+++ app/Exceptions/PersonNotFoundException.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+/**
+ * EntityNotFoundException
+ *
+ * Kivétel akkor történik, ha egy személy nem található
+ *
+ * @author Kovács Zoltán <zoltan1_kovacs@msn.com>
+ */
+class PersonNotFoundException extends Exception
+{
+    /**
+     * A nem található személy azonosítója.
+     *
+     * @var int
+     */
+    protected int $personId;
+/*************  ✨ Codeium Command 🌟  *************/
+    /**
+     * A kivétel megjelenésekor megjelenő üzenet.
+     *
+     * @var string
+     */
+    protected string $message = '';
+/******  9f99798c-d697-44db-bfc1-fba2bcc7c92c  *******/
+
+    // TODO: Nyelvesítés még nincs megoldva
+    public function __construct(int $id) {
+        $this->personId = $id;
+        $this->message = __('exception_person_not_found', ['id' => $id]);
+        // Hívja fel a szülő konstruktort az üzenettel
+        parent::__construct( $this->message );
+    }
+
+    public function report() {
+        // Jelentkezés a tevékenységek naplófájlban
+        // https://spatie.be/docs/laravel-activitylog/v6/basic-usage/logging-activity
+        activity()
+            ->withProperties([
+                'id' => $this->personId, // A keresett személy id-je
+                'message' => $this->getMessage(), // A kivétel üzenete
+            ])
+            ->log( $this->message );
+    }
+}

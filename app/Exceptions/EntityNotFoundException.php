7af2229b757ca777f54a49890a4230af4e5d2ffc
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2024-11-09 18:35:38 +0100

    entities - EntityNotFoundException

--- app/Exceptions/EntityNotFoundException.php
+++ app/Exceptions/EntityNotFoundException.php
@@ -14,13 +14,39 @@ use Exception;
 class EntityNotFoundException extends Exception
 {
     /**
+     * A nem található entitás azonosítója.
+     *
+     * @var int
+     */
+     protected int $entityId;
+    /**
+     * A kivétel megjelenésekor megjelenő üzenet.
+     *
+     * @var string
+     */
+    protected string $message = '';
+
+    /**
      * EntityNotFoundException konstruktor
      *
      * @param int $id A nem található entitás azonosítója
      */
     public function __construct($id)
     {
+        $this->entityId = $id;
+        $this->message = __('exception_entity_not_found', ['id' => $id]);
         // Hívja fel a szülő konstruktort az üzenettel
-        parent::__construct( message: __(key: 'exception_entity_not_found', replace: ['id' => $id]) );
+        parent::__construct( $this->message );
+    }
+
+    public function report() {
+        // Jelentkezés a tevékenységek naplófájlban
+        // https://spatie.be/docs/laravel-activitylog/v6/basic-usage/logging-activity
+        activity()
+            ->withProperties([
+                'id' => $this->entityId, // A keresett entitás id-je
+                'message' => $this->getMessage(), // A kivétel üzenete
+            ])
+            ->log( $this->message );
     }
 }

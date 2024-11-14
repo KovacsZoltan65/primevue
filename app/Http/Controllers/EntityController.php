622139f1a11b2dbca680a5af687d6317020b76ff
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2024-11-14 17:20:48 +0100

    entities - kommentelés

--- app/Http/Controllers/EntityController.php
+++ app/Http/Controllers/EntityController.php
@@ -7,7 +7,9 @@ use App\Http\Requests\UpdateEntityRequest;
 use App\Http\Resources\EntityResource;
 use App\Models\Entity;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Inertia\Inertia;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -17,18 +19,37 @@ class EntityController extends Controller
         //
     }
 
+    /**
+     * Jelenítse meg az entitások listáját.
+     *
+     * Lekéri a név szerint rendezett cégek listáját, és átadja azt,
+     * a keresési paraméterrel együtt a kérelemből az Inertia nézetbe.
+     *
+     * @param Request $request A keresési paramétereket tartalmazó HTTP-kérelem példány.
+     * @return \Inertia\Response Az Inertia válasz a cégek adataival.
+     */
     public function index(Request $request)
     {
         $companies = \App\Models\Company::select('id', 'name')
             ->orderBy('name')->get()->toArray();
-        
+
         return Inertia::render('Entity/Index', [
             'search' => $request->get('search'),
             'companies' => $companies
         ]);
     }
 
-    public function applySearch(Builder $query, string $search)
+    /**
+     * Módosítsa a lekérdezést a keresési paraméter alapján.
+     *
+     * Ha a keresési paraméter nem üres, akkor a lekérdezés tartalmazza
+     * a feltételt, hogy a vállalat neve tartalmazza a keresési paramétert.
+     *
+     * @param Builder $query A lekérdezés, amelyet módosítani kell.
+     * @param string $search A keresési paraméter.
+     * @return Builder A módosított lekérdezés.
+     */
+    public function applySearch(Builder $query, string $search): Builder
     {
         return $query->when($search, function($query, string $search) {
             // Adjon hozzá egy where záradékot a lekérdezéshez azzal a feltétellel, hogy a
@@ -37,7 +58,17 @@ class EntityController extends Controller
         });
     }
 
-    public function getEntities(Request $request)
+    /**
+     * Szerezd meg az entitások listáját a keresési paraméter alapján.
+     *
+     * Ez a módszer a keresési lekérdezést az adatbázis-lekérdezésre alkalmazza.
+     * Ha a keresési lekérdezés nem üres, akkor egy where záradékot ad a lekérdezéshez
+     * azzal a feltétellel, hogy az entitás nevének tartalmaznia kell a keresési lekérdezést.
+     *
+     * @param  \Illuminate\Http\Request  $request  A HTTP kérés objektum, amely tartalmazza a keresési paramétereket.
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection Az entitások listáját tartalmazó JSON-válasz.
+     */
+    public function getEntities(Request $request): AnonymousResourceCollection
     {
         $entityQuery = Entity::Search($request);
         $entities = EntityResource::collection($entityQuery->get());
@@ -45,29 +76,55 @@ class EntityController extends Controller
         return $entities;
     }
 
-    public function getEntity(int $id)
+    /**
+     * Visszaadja a megadott azonosítóval rendelkező entitást.
+     *
+     * @param int $id Az entitás azonosítója.
+     * @return \Illuminate\Http\JsonResponse A keresett entitás adatait tartalmazó JSON-válasz.
+     */
+    public function getEntity(int $id): JsonResponse
     {
         $entity = Entity::find($id);
 
         return response()->json($entity, Response::HTTP_OK);
     }
 
-    public function getEntityByName(string $name)
+    /**
+     * Visszaadja a megadott névvel rendelkező entitást.
+     *
+     * @param string $name Az entitás neve.
+     * @return \Illuminate\Http\JsonResponse A keresett entitás adatait tartalmazó JSON-válasz.
+     */
+    public function getEntityByName(string $name): JsonResponse
     {
         $entity = Entity::where('name', $name)->first();
 
         return response()->json($entity, Response::HTTP_OK);
     }
 
-    public function createEntity(StoreEntityRequest $request)
+    /**
+     * Hozzon létre egy új entitást.
+     *
+     * @param StoreEntityRequest $request A HTTP kérés objektum, amely tartalmazza az új entitás adatait.
+     * @return \Illuminate\Http\JsonResponse Az újonnan létrehozott entitás adatait tartalmazó JSON-válasz.
+     */
+    public function createEntity(StoreEntityRequest $request): JsonResponse
     {
-        //\Log::info('$request->all(): ', $request->all());
         $entity = Entity::create($request->all());
-\Log::info('$entity' . print_r($entity, true));
+
         return response()->json($entity, Response::HTTP_OK);
     }
 
-    public function updateEntity(UpdateEntityRequest $request, int $id)
+    /**
+     * Módosít egy entitást az adatbázisban.
+     *
+     * A módosított entitás sikerességét jelző JSON-válasz kerül visszaadásra.
+     *
+     * @param  \App\Http\Requests\UpdateEntityRequest  $request  A HTTP kérés objektum, amely tartalmazza a módosítani kívánt entitás adatait.
+     * @param  int  $id  A módosítani kívánt entitás azonosítója.
+     * @return \Illuminate\Http\JsonResponse A sikerességet jelző JSON-válasz.
+     */
+    public function updateEntity(UpdateEntityRequest $request, int $id): JsonResponse
     {
         $old_entity = Entity::find($id);
 
@@ -76,7 +133,15 @@ class EntityController extends Controller
         return response()->json(['success' => $success], Response::HTTP_OK);
     }
 
-    public function deleteEntity(int $id)
+    /**
+     * Töröljön egy entitást az adatbázisból.
+     *
+     * A törölt entitás sikerességét jelző JSON-válasz kerül visszaadásra.
+     *
+     * @param int $id A törölni kívánt entitás azonosítója.
+     * @return \Illuminate\Http\JsonResponse A sikerességet jelző JSON-válasz.
+     */
+    public function deleteEntity(int $id): JsonResponse
     {
         $entity = Entity::find($id);
         $success = $entity->delete();
@@ -93,7 +158,7 @@ class EntityController extends Controller
      * @param Request $request A törölni kívánt azonosítókkal rendelkező „entitások” tömböt tartalmazó HTTP-kérés.
      * @return \Illuminate\Http\JsonResponse JSON-válasz, amely jelzi a sikeres állapotot és a törölt entitások számát.
      */
-    public function deleteEntities(Request $request)
+    public function deleteEntities(Request $request): JsonResponse
     {
         // Gyűjtsük ki a kérésből a törlendő entitások azonosítóit.
         // A $request->input('entities') egy tömb, amelyben az egyes elemek az
@@ -109,7 +174,7 @@ class EntityController extends Controller
             // Ha a tömb üres, akkor a kérés nem tartalmazott érvényes azonosítókat,
             // ezért visszaadjuk egy 400-as hibaüzenetet.
             return response()->json([
-                'success' => false, 
+                'success' => false,
                 'message' => 'No valid IDs provided'
             ], Response::HTTP_BAD_REQUEST);
         }
@@ -131,7 +196,7 @@ class EntityController extends Controller
         $success = $deleted > 0;
 
         return response()->json([
-            'success' => $success, 
+            'success' => $success,
             'deleted_count' => $deleted
         ], Response::HTTP_OK);
     }

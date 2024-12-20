54d21ad58cb62316f6c16829b04f57e302bea4a5
Kovács Zoltán <kovacs.zoltan@softc.hu>
2024-12-20 08:35:55 +0100

    repository - Profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,14 +9,33 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
-use Inertia\Response;
+use Inertia\Response AS InertiaResponse;
+use App\Traits\Functions;
+use App\Repositories\ProfileRepository;
+use Illuminate\Routing\Controller;
 
 class ProfileController extends Controller
 {
+    use AuthorizesRequests,
+        Functions;
+
+    protected ProfileRepository $profileRepository;
+
+    public function __construct(ProfileRepository $repository)
+    {
+        $this->profileRepository = $repository;
+
+        //$this->middleware('can:profiles list', ['only' => ['index', 'applySearch', 'getProfiles', 'getProfile', 'getProfileByName']]);
+        //$this->middleware('can:profiles create', ['only' => ['createProfile']]);
+        //$this->middleware('can:profiles edit', ['only' => ['updateProfile']]);
+        //$this->middleware('can:profiles delete', ['only' => ['deleteProfile', 'deleteProfiles']]);
+        //$this->middleware('can:profiles restore', ['only' => ['restoreProfile']]);
+    }
+
     /**
      * Display the user's profile form.
      */
-    public function edit(Request $request): Response
+    public function edit(Request $request): InertiaResponse
     {
         return Inertia::render('Profile/Edit', [
             'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
@@ -29,6 +48,9 @@ class ProfileController extends Controller
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
+        $this->profileRepository->updateProfile($request->user(), $request->validated();
+
+        /*
         $request->user()->fill($request->validated());
 
         if ($request->user()->isDirty('email')) {
@@ -36,7 +58,7 @@ class ProfileController extends Controller
         }
 
         $request->user()->save();
-
+        */
         return Redirect::route('profile.edit');
     }
 
@@ -53,7 +75,8 @@ class ProfileController extends Controller
 
         Auth::logout();
 
-        $user->delete();
+        //$user->delete();
+        $this->profileRepository->deleteUserAccount($user);
 
         $request->session()->invalidate();
         $request->session()->regenerateToken();

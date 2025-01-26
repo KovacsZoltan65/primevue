17c625e71761a8a82444f40aa42329c6bcab9cff
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2025-01-26 18:15:54 +0100

    main - seed jó

--- database/seeders/EntitySeeder.php
+++ database/seeders/EntitySeeder.php
@@ -17,13 +17,14 @@ class EntitySeeder extends Seeder
      */
     public function run(): void
     {
+        echo 'EntitySeeder' . PHP_EOL;
         Schema::disableForeignKeyConstraints();
         Entity::truncate();
         Schema::enableForeignKeyConstraints();
-        
+
         // Logolás letiltása
         activity()->disableLogging();
-        
+
         $faker = Factory::create();
         $company_ids = Company::pluck('id')->toArray();
         $person_ids = Person::pluck('id')->toArray();
@@ -31,57 +32,31 @@ class EntitySeeder extends Seeder
         $arr_entities = array_map(function ($i) use ($faker, $company_ids, $person_ids): array {
             return [
                 'id' => $i,
-                'name' => "Entity_{$i}",
+                'name' => "Entity_" . ($i < 10) ? "0{$i}" : $i,
                 'email' => "entity_{$i}@company.com",
+                'email' => 'entity_' . ($i < 10) ? "0{$i}" : $i . '@company.com',
                 'company_id' => $faker->randomElement($company_ids),
-                //'persons_id' => $faker->randomElement($person_ids),
                 'start_date' => $faker->dateTimeBetween('-10 year', 'now'),
                 'end_date' => $faker->dateTimeBetween('now', '+10 year'),
                 'last_export' => NULL,
                 'active' => 1,
             ];
         }, range(1, 40));
-        
+
         $count = count($arr_entities);
-        
+
         $this->command->warn(PHP_EOL . __('migration_creating_entities') );
         $this->command->getOutput()->progressStart($count);
-        
-        foreach($arr_entities as $entity)
-        {
+
+        foreach($arr_entities as $entity) {
             Entity::create($entity);
             $this->command->getOutput()->progressAdvance();
         }
 
         $this->command->getOutput()->progressFinish();
-        
+
         $this->command->info(PHP_EOL . __('migration_created_entities') );
 
-        // Hierarchia kapcsolatok létrehozása
-        $this->createInitialHierarchy();
-        
         activity()->enableLogging();
-        
-    }
-    
-    /**
-     * Hierarchia első kapcsolatainak létrehozása
-     */
-    private function createInitialHierarchy(): void
-    {
-        $this->command->warn(__('Creating initial hierarchy relationships...'));
-
-        // Példa: Az első entitás a "nagyfőnök", a következő 5 entitás gyermeke lesz
-        $parent = Entity::first(); // Az első entitás
-        $children = Entity::where('id', '>', $parent->id)->take(5)->get();
-
-        foreach ($children as $child) {
-            Hierarchy::create([
-                'parent_id' => $parent->id,
-                'child_id' => $child->id,
-            ]);
-        }
-
-        $this->command->info(__('Initial hierarchy relationships created.'));
     }
 }

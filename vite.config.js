8a2f06ba06d1c5a55a3e57d3b62e7a0ab0233a53
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2025-01-28 17:02:07 +0100

    main - mentés

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+    server: {
+        cors: true, // Engedélyezi a CORS-t
+        host: '127.0.0.1', // IPv4-re állítás
+        port: 5173
+    },
     plugins: [
         laravel({
             input: "resources/js/app.js",
